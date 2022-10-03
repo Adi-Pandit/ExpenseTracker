496eb191747c1db6b60296c63ac681d0e07d5a9b
Adi-Pandit <adityapandit705@gmail.com>
2022-10-04 00:40:07 +0530

    init

---
+++ ExpenseTracker/authentication/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
+from django.contrib import messages
+
+# Create your views here.
+
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
+    
+    def post(self, request):
+       #GET USER DATA
+       #VALIDATE
+       #CREATE A USER ACCOUNT
+
+       username = request.POST['username']
+       email = request.POST['email']
+       password = request.POST['password']
+       return render(request, 'authentication/register.html')
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error':'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error':'sorry username in use, choose another username'}, status=409)
+        return JsonResponse({'username_valid':True})
+
+class EmailValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'sorry email in use, choose another email'}, status=409)
+        return JsonResponse({'email_valid':True})
