a7e2842b06515619d66bfa450719406790603972
Adi-Pandit <adityapandit705@gmail.com>
2022-10-09 12:36:03 +0530

    name change

--- ExpenseTracker/authentication/views.py
+++ ExpenseTracker/authentication/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
 from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from .utils import account_activation_token
 
 # Create your views here.
 
@@ -34,7 +40,29 @@ class RegistrationView(View):
                 
                 user= User.objects.create_user(username=username,email=email)
                 user.set_password(password)
+                user.is_active = False
                 user.save()
+
+                #path_to_view
+                # - getting domain we are on
+                # - relative url to verification
+                # - encode uid
+                # - token
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+
+                domain = get_current_site(request).domain
+                link = reverse('activate',kwargs={'uidb64': uidb64, 'token': token_generator.make_token(user)})
+
+                activate_url = 'http://'+domain+link
+                email_subject = 'Activate your account'
+                email_body = 'Hi '+user.username+' Please use this link to verify your acount\n' + activate_url
+                email = EmailMessage(
+                        email_subject,
+                        email_body,
+                        'noreply@semycolon.com',
+                        [email],
+                    )
+                email.send(fail_silently=False)
                 messages.success(request, 'Account successfully created')
                 return render(request, 'authentication/register.html')
 
@@ -63,3 +91,28 @@ class EmailValidationView(View):
         if User.objects.filter(email=email).exists():
             return JsonResponse({'email_error':'sorry email in use, choose another email'}, status=409)
         return JsonResponse({'email_valid':True})
+
+class VerificationView(View):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not account_activation_token.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request,'Account activated successfully')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+        return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
