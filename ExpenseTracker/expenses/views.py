dda0cc98d3ff8617577dbadf340e10e32d9129c6
Adi-Pandit <adityapandit705@gmail.com>
2022-10-11 00:21:20 +0530

    name change

--- ExpenseTracker/expenses/views.py
+++ ExpenseTracker/expenses/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category,Expense
+from django.contrib import messages
 # Create your views here.
 
 
@@ -8,4 +10,31 @@ def index(request):
     return render(request, 'expenses/index.html')
 
 def add_expenses(request):
-    return render(request, 'expenses/add_expenses.html')
+    categories = Category.objects.all()
+    context={
+            'categories': categories,
+            'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expenses.html',context)
+
+    if request.method == 'POST':
+        amount=request.POST['amount']
+
+        if not amount:
+            messages.error(request,'Amount is required')
+            return render(request, 'expenses/add_expenses.html',context)
+
+        description=request.POST['description']
+        date=request.POST['expense_date']
+        category=request.POST['category']
+
+        if not description:
+            messages.error(request,'Description is required')
+            return render(request, 'expenses/add_expenses.html',context)
+
+        
+
+        Expense.objects.create(owner=request.user,amount=amount,date=date,category=category,description=description)
+        messages.success(request,'Expense saved successfully')
+        return redirect('expenses')
