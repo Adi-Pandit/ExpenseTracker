a2dc986685eb8dfca815e6a89c000c70baa4e619
Adi-Pandit <adityapandit705@gmail.com>
2022-10-23 18:07:13 +0530

    name change

--- ExpenseTracker/ExpenseTracker/static/js/searchExpenses.js
+++ ExpenseTracker/ExpenseTracker/static/js/searchExpenses.js
@@ -2,8 +2,9 @@ const searchField = document.querySelector("#searchField");
 const tableOutput = document.querySelector(".table-output");
 const appTable = document.querySelector(".app-table");
 const paginationContainer = document.querySelector(".pagination-container");
-const tbody = document.querySelector(".table-body");
 tableOutput.style.display = 'none';
+const noResults = document.querySelector(".no-results");
+const tbody = document.querySelector(".table-body");
 
 searchField.addEventListener('keyup', (e) => {
     const searchValue = e.target.value;
@@ -20,20 +21,21 @@ searchField.addEventListener('keyup', (e) => {
                 console.log("data", data);
                 appTable.style.display = "none";
                 tableOutput.style.display = "block";
-
+                console.log("data.length", data.length);
                 if (data.length === 0) {
-                    tableOutput.innerHTML = "No result found";
+                    noResults.style.display = "block";
+                    tableOutput.style.display = "none";
                 }
                 else {
-
+                    noResults.style.display = "none";
                     data.forEach((item) => {
                         tbody.innerHTML += `
-                    <tr>
-                        <td>${item.amount}</td>
-                        <td>${item.category}</td>
-                        <td>${item.description}</td>
-                        <td>${item.date}</td>
-                    </tr>`;
+                            <tr>
+                                <td>${item.amount}</td>
+                                <td>${item.category}</td>
+                                <td>${item.description}</td>
+                                <td>${item.date}</td>
+                            </tr>`;
 
                     });
                 }
