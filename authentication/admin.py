36ece94c410a7c96f5c699153bf40f5896feb6e1
Adi-Pandit <adityapandit705@gmail.com>
2026-06-18 20:54:13 +0530

    Register User model in admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "is_staff", "is_active"]
+    list_filter = ["is_staff", "is_active"]
+    search_fields = ["username", "email", "first_name", "last_name"]
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("base_currency",)}),
+    )
